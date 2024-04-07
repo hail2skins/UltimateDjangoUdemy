c81328b45c36381293840b635464a2b9b6e61cf7
Art Mills <art@hamcois.com>
2024-04-07 18:03:37 -0500

    variables and loops for templates

--- elevate/crm/views.py
+++ elevate/crm/views.py
@@ -4,7 +4,40 @@ from django.http import HttpResponse # Add this import to allow for an HTTP resp
 # Create your views here.
 
 def home(request):
-    return render(request, 'crm/index.html') # Add this function to render the index.html template
+    
+    client_list = [
+        {
+            'id': 1,
+            'name': 'John Doe',
+            'occupation': 'Web Developer',
+        },
+        {
+            'id': 2,
+            'name': 'Kate Smith',
+            'occupation': 'Attorney',
+        },
+    ]
+    
+    examList = [
+        {
+            'id': 1,
+            'name': 'Science',
+            'grade': '12',
+        },
+        {
+            'id': 2,
+            'name': 'Math',
+            'grade': '10',
+        },
+    ]
+    
+    context = {
+        'first_name': 'Art',
+        'client_list': client_list,
+        'examList': examList,
+    }
+    
+    return render(request, 'crm/index.html', context) # Add this function to render the index.html template
 
 
 def register(request):
